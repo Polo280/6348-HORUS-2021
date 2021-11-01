65f2a82b3f56cfae24ce2e5efa2ba410aab0f3ff
Jorge Gonzalez <jorglezd28@gmail.com>
2021-11-01 11:38:55 -0600

    Agregue el codigo de drivetrain de marselo2 y su respectiva clase funcionales

---
+++ clases/drivetrain.py
@@ -0,0 +1,31 @@
+import wpilib
+import rev
+from math import fabs
+from wpilib.drive import DifferentialDrive
+
+class DriveTrain():
+    def __init__(self, right_f: rev.CANSparkMax, right_b: rev.CANSparkMax, left_f: rev.CANSparkMax, left_b: rev.CANSparkMax):
+        self.right_f = right_f
+        self.left_f = left_f
+        self.right_b = right_b
+        self.left_b = left_b
+
+        self.right = wpilib.SpeedControllerGroup(self.right_f, self.right_b)
+        self.left = wpilib.SpeedControllerGroup(self.left_f, self.left_b)
+        self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
+
+    def manejar(self, xbox1):
+        x = -(xbox1.getX(wpilib.XboxController.Hand.kLeftHand))
+        y = xbox1.getY(wpilib.XboxController.Hand.kLeftHand)
+
+        R1 = (100 - fabs(x)) * (y / 100) + y  # R + L
+        R2 = (100 - fabs(y)) * (x / 100) + x  # R - L
+        right = (R1 + R2) / 2
+        left = (R1 - R2) / 2
+
+        trigger_left = xbox1.getTriggerAxis(wpilib.XboxController.Hand.kLeftHand)
+        trigger_right = xbox1.getTriggerAxis(wpilib.XboxController.Hand.kRightHand)
+        multiplicador = (trigger_left * 0.7) + (trigger_right * 0.3)
+
+        self.drive.tankDrive(left * multiplicador, right * multiplicador)
+
---
+++ robot.py
@@ -0,0 +1,28 @@
+import wpilib
+import rev
+from drivetrain import DriveTrain
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.neo_rf = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.neo_lf = rev.CANSparkMax(2, rev.MotorType.kBrushless)
+        self.neo_rb = rev.CANSparkMax(3, rev.MotorType.kBrushless)
+        self.neo_lb = rev.CANSparkMax(4, rev.MotorType.kBrushless)
+        self.chasis = DriveTrain(self.neo_rf, self.neo_rb, self.neo_lf, self.neo_lb)
+
+        self.xbox1= wpilib.XboxController(0)
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        self.chasis.manejar(self.xbox1)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
