a03e24179014ce87e0105375e42007009beb22fc
Jorge Gonzalez <jorglezd28@gmail.com>
2021-11-13 13:55:09 -0600

    Implementacion experimental de shooter y limelight

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@
 import wpilib
 import rev
 from Clases.drivetrain import DriveTrain
+from Clases.limelight import LimelightCam
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
@@ -11,19 +12,33 @@ class MyRobot(wpilib.TimedRobot):
         self.neo_lb = rev.CANSparkMax(4, rev.MotorType.kBrushless)
         self.chasis = DriveTrain(self.neo_rf, self.neo_rb, self.neo_lf, self.neo_lb)
 
+        self.shooter = rev.CANSparkMax(5, rev.MotorType.kBrushless)
+        self.camera = LimelightCam()
         self.xbox1= wpilib.XboxController(0)
 
     def autonomousInit(self):
         pass
 
     def autonomousPeriodic(self):
-        pass
+        movedir = self.camera.grabar()
+        self.chasis.move(movedir)
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
         self.chasis.manejar(self.xbox1)
+
+        if self.xbox1.getAButton() and self.xbox1.getBButton() == False:
+            self.shooter.set(0.8)
+
+        elif self.xbox1.getAButton() == False and self.xbox1.getBButton():
+            self.shooter.set(-0.8)
+            
+        else:
+            self.shooter.set(0)
+
+
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
